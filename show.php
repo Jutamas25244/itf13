cdebe23de4d3f8d8d01915b0564b62397e61953a
Juramas <63070022@kmitl.ac.th>
2020-11-17 19:00:27 +0700

    add bootstrap

--- show.php
+++ show.php
@@ -1,9 +1,13 @@
 <html>
+
 <head>
-<title>ITF Lab</title>
+  <title>ITF Lab</title>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+  <link rel="stylesheet" href="style.css">
 </head>
+
 <body>
-<?php
+  <?php
 $conn = mysqli_init();
 mysqli_real_connect($conn, 'labitf13.mysql.database.azure.com', 'Jutamas@labitf13', 'DMJbvc45', 'itflab', 3306);
 if (mysqli_connect_errno($conn))
@@ -12,27 +16,35 @@ if (mysqli_connect_errno($conn))
 }
 $res = mysqli_query($conn, 'SELECT * FROM guestbook');
 ?>
-<table width="600" border="1">
-  <tr>
-    <th width="100"> <div align="center">Name</div></th>
-    <th width="350"> <div align="center">Comment </div></th>
-    <th width="150"> <div align="center">Link </div></th>
-  </tr>
-<?php
+  <table width="600" border="1">
+    <tr>
+      <th width="100">
+        <div align="center">Name</div>
+      </th>
+      <th width="350">
+        <div align="center">Comment </div>
+      </th>
+      <th width="150">
+        <div align="center">Link </div>
+      </th>
+    </tr>
+    <?php
 while($Result = mysqli_fetch_array($res))
 {
 ?>
-  <tr>
-    <td><?php echo $Result['Name'];?></div></td>
-    <td><?php echo $Result['Comment'];?></td>
-    <td><?php echo $Result['Link'];?></td>
-  </tr>
-<?php
+    <tr>
+      <td><?php echo $Result['Name'];?></div>
+      </td>
+      <td><?php echo $Result['Comment'];?></td>
+      <td><?php echo $Result['Link'];?></td>
+    </tr>
+    <?php
 }
 ?>
-</table>
-<?php
+  </table>
+  <?php
 mysqli_close($conn);
 ?>
 </body>
+
 </html>
