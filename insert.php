ef9b04a7e5c8741c74fe1453ed12c2efb5bd805d
Juramas <63070022@kmitl.ac.th>
2020-11-17 20:36:17 +0700

    ตกแต่ง

--- insert.php
+++ insert.php
@@ -17,10 +17,12 @@ $sql = "INSERT INTO guestbook (Name , Comment , Link) VALUES ('$name', '$comment
 
 
 if (mysqli_query($conn, $sql)) {
-    echo "New record created successfully";
+    echo "Data has been SAVED.";
+?>
+  <a href="form.html">Form</a> | <a href="show.php">Show</a>
+<?php
   } else {
     echo "Error: " . $sql . "<br>" . mysqli_error($conn);
   }
-  
 mysqli_close($conn);
 ?>
--- show.php
+++ show.php
@@ -1,3 +1,12 @@
+<?php
+$conn = mysqli_init();
+mysqli_real_connect($conn, 'labitf13.mysql.database.azure.com', 'Jutamas@labitf13', 'DMJbvc45', 'itflab', 3306);
+if (mysqli_connect_errno($conn)){
+    die('Failed to connect to MySQL: '.mysqli_connect_error());
+}
+$res = mysqli_query($conn, 'SELECT * FROM guestbook');
+?>
+
 <html>
 
 <head>
@@ -7,44 +16,41 @@
 </head>
 
 <body>
-  <?php
-$conn = mysqli_init();
-mysqli_real_connect($conn, 'labitf13.mysql.database.azure.com', 'Jutamas@labitf13', 'DMJbvc45', 'itflab', 3306);
-if (mysqli_connect_errno($conn))
-{
-    die('Failed to connect to MySQL: '.mysqli_connect_error());
-}
-$res = mysqli_query($conn, 'SELECT * FROM guestbook');
-?>
-  <table width="600" border="1">
-    <tr>
-      <th width="100">
-        <div align="center">Name</div>
-      </th>
-      <th width="350">
-        <div align="center">Comment </div>
-      </th>
-      <th width="150">
-        <div align="center">Link </div>
-      </th>
-    </tr>
-    <?php
-while($Result = mysqli_fetch_array($res))
-{
-?>
-    <tr>
-      <td><?php echo $Result['Name'];?></div>
-      </td>
-      <td><?php echo $Result['Comment'];?></td>
-      <td><?php echo $Result['Link'];?></td>
-    </tr>
-    <?php
-}
-?>
-  </table>
-  <?php
+  <div class="page-show">
+    <div class="container-fluid main-container">
+      <div class="row">
+        <div class="col">
+          <table class="table">
+            <thead>
+              <tr>
+                <th width="20%">
+                  <div align="center" class="text">Name</div>
+                </th>
+                <th>
+                  <div align="center" class="text">Comment </div>
+                </th>
+                <th width="30%">
+                  <div align="center" class="text">Link </div>
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              <?php while($Result = mysqli_fetch_array($res)) : ?>
+              <tr>
+                <td><?php echo $Result['Name'];?></div>
+                </td>
+                <td><?php echo $Result['Comment'];?></td>
+                <td><?php echo $Result['Link'];?></td>
+              </tr>
+              <?php endwhile; ?>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  </div>
+</body>
+</html>
+<?php
 mysqli_close($conn);
 ?>
-</body>
-
-</html>
